b12ca89c73635073464620048fffeb67aa01aa03
uksalinho <uksaloguzhan@gmail.com>
2022-10-05 21:23:29 +0200

    Update exercise.py

---
+++ 2- Weekly Agendas/Week_23/exercise.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from PIL import Image
+
+st.title("Streamlit Tutorials")
+
+st.text("Hello Streamlit")
+
+#### Header/Subheader ####
+st.header("This is a header")
+st.subheader("This is a subheader")
+
